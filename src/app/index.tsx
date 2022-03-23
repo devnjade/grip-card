b3c24b9e35ddd06bfce5d3172f06e2b2c7c488bf
Adebayo Temitope <adecancode@gmail.com>
2022-03-23 01:34:56 +0100

    changes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,28 @@
 import React from "react";
+import styles from './index.module.scss'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div className={styles.main}>
+      <div className={styles.first}>
+        <div className={styles.header}>
+
+        </div>
+        <div className={styles.logo}>
+
+        </div>
+        <div className={styles.footer}>
+
+        </div>
+      </div>
+      <div className={styles.second}>
+
+      </div>
+      <div className={styles.third}>
+        <div className={styles.barcode}>
+
+        </div>
+      </div>
     </div>
   );
 }
